ac8daca34512e9c43b28aac04a8716623ccb5067
SN <naidu.shristhy@live.com>
2014-12-04 05:33:58 -0800

    committt

---
+++ southeast.c
@@ -0,0 +1,31 @@
+// Shristhy Naidu
+// The Southeast Program
+
+#include <stdio.h>
+
+void go_south_east(int* lat, int* lon)
+{
+	*lat = *lat - 1;
+	*lon = *lon + 1;
+
+	//lat--;
+	//lon++;
+
+}
+
+int main (void)
+{
+	int latitude = 32;
+	int longitude = 64;
+
+	go_south_east(latitude, longitude);
+
+	printf("Now at %i %i\n", latitude, longitude);
+
+
+
+
+
+
+	return 0;
+}
