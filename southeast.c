a3d4ad472fb779768c5b8943b7821ccdfabd2559
SN <naidu.shristhy@live.com>
2014-12-04 05:50:24 -0800

    Second Version

--- southeast.c
+++ southeast.c
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-void go_south_east(int lat, int lon)
+void go_south_east(int* lat, int* lon)
 {
 	*lat = *lat - 1;
 	*lon = *lon + 1;
@@ -18,7 +18,7 @@ int main (void)
 	int latitude = 32;
 	int longitude = -64;
 
-	go_south_east(latitude, longitude);
+	go_south_east(&latitude, &longitude);
 
 	printf("Avast! Now at: [%i %i]\n", latitude, longitude);
 
